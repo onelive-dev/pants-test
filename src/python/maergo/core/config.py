4b59c47c4da25c4d65fb1bde00973db1e9d2fa36
Dylan Warnock <dylan.warnock@xdelivery.ai>
2022-12-08 11:05:14 -0800

    update

--- src/python/maergo/core/config.py
+++ src/python/maergo/core/config.py
@@ -1,10 +1,21 @@
 import os
 
+from enum import Enum
+
 from pydantic import BaseSettings
 
 
+class Env(Enum):
+    DEV = "dev"
+    STAGE = "stg"
+    PROD = "prod"
+
+
 class Config(BaseSettings):
+    ENV = os.getenv("ENV", "dev")
+    PORT = int(os.getenv("PORT", "8000"))
     API_V1_PREFIX = "/v1"
+    POSTGRES_CONNECTION_URL = os.getenv("POSTGRES_CONNECTION_URL", "mongodb://mongodb/")
 
 
 config = Config()
--- src/python/maergo/main.py
+++ src/python/maergo/main.py
@@ -5,7 +5,7 @@ from fastapi.responses import RedirectResponse
 from .routers.v1 import v1_organizations
 from .core.config import config
 
-app = FastAPI(title="Maergo")
+app = FastAPI(title="Maergo1")
 app.include_router(v1_organizations.router, prefix=config.API_V1_PREFIX)
 
 
@@ -15,4 +15,4 @@ async def docs_redirect() -> RedirectResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=config.PORT)
